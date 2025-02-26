715fe7faa51d513771fc6ba9b32b1c128d8c0836
Saidxoja404 <trashXshorts099@gmail.com>
2025-02-26 23:20:35 +0500

    men confliktga qarshiman

---
+++ pdp-task1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PDPtask1
+{
+    class Board
+    {
+        public char[,] board =
+        {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' }
+        };
+
+        public void DrawBoard()
+        {
+            Console.WriteLine("  {0} | {1} | {2} ", board[0, 0], board[0, 1], board[0, 2]);
+            Console.WriteLine(" ---+---+---");
+            Console.WriteLine("  {0} | {1} | {2} ", board[1, 0], board[1, 1], board[1, 2]);
+            Console.WriteLine(" ---+---+---");
+            Console.WriteLine("  {0} | {1} | {2} ", board[2, 0], board[2, 1], board[2, 2]);
+        }
+    }
+}
