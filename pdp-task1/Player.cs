458b50c4ede0e798a5f700a419258649466a6e45
Cs2Gamer <mirqobilovmahmudjon6@gmail.com>
2025-02-26 11:23:09 -0700

    board and player

---
+++ pdp-task1/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pdptask1
+{
+
+    class Player
+    {
+        public char currentPlayer = 'X';
+
+        public void SwitchPlayer()
+        {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+}
--- pdp-task1/Program.cs
+++ pdp-task1/Program.cs
@@ -1,5 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using pdptask1;
+using PDPtask1;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Board board = new Board();
+        board.DrawBoard();
+
+        Player player = new Player();
+        player.SwitchPlayer();
+    }
+}
